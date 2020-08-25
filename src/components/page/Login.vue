90dd42ee0640edd5418859bfb39f84087e36e4f9
ludan <1107743896@qq.com>
2020-08-25 11:30:17 +0800

    修改登陆

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -47,7 +47,7 @@ export default {
                 if (valid) {
                     this.$message.success('登录成功');
                     localStorage.setItem('ms_username', this.param.username);
-                    this.$router.push('/');
+                    this.$router.push('/dashboard');
                 } else {
                     this.$message.error('请输入账号和密码');
                     console.log('error submit!!');
