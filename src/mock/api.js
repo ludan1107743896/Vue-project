182b4bd9ccd2cf85eef73283542b26509d668a1d
ludan <1107743896@qq.com>
2020-08-25 16:03:53 +0800

    mock 方法封装

--- src/mock/api.js
+++ src/mock/api.js
@@ -1,26 +1,28 @@
-// 引入mockjs
 const Mock = require('mockjs')
-
-// 获取 mock.Random 对象
 const Random = Mock.Random
 
-// mock一组数据
-const simulateData = function () {
-  let data1 = []
-  for (let i = 0; i < 8; i++) {
-    let data = {
-      name: Random.cname(), 
-      date: Random.date(), 
-      city: Random.city(),
-      img: Random.image(), 
-      content: Random.cparagraph(),
+
+const getDataSource = (num, _obj, type) => {
+  let dataItem = type == 'object' ? {} : [];
+  if(type == 'object'){
+    return _obj;
+  } else {
+    for (let i = 0; i< num; i++){
+      dataItem.push(_obj);
     }
-    data1.push(data)
   }
-  
-  return {
-    data: data1
-  }
+  return {data: dataItem};
 }
+
+// mock一组数据
+const simulateData = getDataSource(8, {
+  name: Random.cname(), 
+  date: Random.date(), 
+  city: Random.city(),
+  img: Random.image(), 
+  content: Random.cparagraph(),
+  id: Random.guid()
+})
+
 
 Mock.mock('/user/info', 'get', simulateData)
