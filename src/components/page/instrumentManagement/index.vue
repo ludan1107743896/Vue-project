8323cf857116cef026e3c0047dad3bc257d85891
ludan <1107743896@qq.com>
2020-08-25 16:42:28 +0800

    仪器维护

--- src/components/page/instrumentManagement/index.vue
+++ src/components/page/instrumentManagement/index.vue
@@ -1,5 +1,26 @@
 <template>
     <div>
-        仪器管理界面
+        <bread-crumb :titleList="title"></bread-crumb>
+        <div class="container">
+            <search-comp />
+            
+        </div>    
     </div>
 </template>
+
+<script>
+import breadCrumb from '../storeManagement/breadcrumb.vue';
+import searchComp from './searchComp.vue';
+
+export default {
+    data() {
+        return {
+            title: ['首页', '仪器管理'],
+        }
+    },
+    components: {
+        breadCrumb,
+        searchComp
+    }
+}
+</script>
---
+++ src/components/page/instrumentManagement/searchComp.vue
@@ -0,0 +1,35 @@
+<template>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="店铺">
+            <el-input v-model="formInline.shop" placeholder="请输入店铺名称"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
+            <el-select v-model="formInline.status" placeholder="选择状态">
+                <el-option label="全部" value="all"></el-option>
+                <el-option label="上线" value="up"></el-option>
+                <el-option label="下线" value="down"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            formInline: {
+                shop: '',
+                status: ''
+            }
+        };
+    },
+    methods: {
+        onSubmit() {
+            console.log('submit!');
+        }
+    }
+};
+</script>
