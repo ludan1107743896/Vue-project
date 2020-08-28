2b16ad0c017716f3eda9a5fb68d0a83fc703dcc2
ludan <1107743896@qq.com>
2020-08-28 11:10:03 +0800

    表单提交

--- src/components/page/customerInquiry/components/BasicInfoComp.vue
+++ src/components/page/customerInquiry/components/BasicInfoComp.vue
@@ -54,11 +54,34 @@
                 </template>
             </el-table-column>
             <el-table-column label="操作">
-                <template>
-                    <el-button type="primary">详情</el-button>
+                <template slot-scope="scope">
+                    <el-button type="primary" @click="handleDeal(scope.row)">详情</el-button>
                 </template>
             </el-table-column>
         </el-table>
+        <el-dialog title="编辑" :visible.sync="modalVsible" :close-on-press-escape="false" :show-close="false">
+            <el-form :model="form" ref="ruleForm" :rules="rules" class="form-box">
+                <el-form-item label="名称" :label-width="formLabelWidth" prop="name">
+                    <el-input v-model="form.name" autocomplete="off" placeholder="请输入名称" />
+                </el-form-item>
+                <el-form-item label="卡号" :label-width="formLabelWidth" prop="cardnumber">
+                    <el-input v-model="form.cardnumber" autocomplete="off" placeholder="请输入卡号" />
+                </el-form-item>
+                <el-form-item label="手机号" :label-width="formLabelWidth" prop="phoneNum">
+                    <el-input v-model="form.phoneNum" autocomplete="off" placeholder="请输入手机号" />
+                </el-form-item>
+                <el-form-item label="性别" :label-width="formLabelWidth" prop="six">
+                    <el-radio-group v-model="form.six">
+                        <el-radio label="1">男</el-radio>
+                        <el-radio label="0">女</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="handleOnok">提 交</el-button>
+                <el-button @click="handleOnCanCel">取 消</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 <script>
@@ -67,18 +90,59 @@ export default {
     data() {
         return {
             tableData: [],
+            modalVsible: false,
+            formLabelWidth: '120px',
             form: {
                 name: '',
-                resource: 'woman',
-                date1: '',
-                date2: ''
+                cardnumber: '',
+                phoneNum: '',
+                six: ''
+            },
+            rules: {
+                name: [{ required: true, message: '请输入名称', trigger: 'blur' }],
+                cardnumber: [{ required: true, message: '请输入卡号', trigger: 'blur' }],
+                phoneNum: [{ required: true, message: '请输入电话', trigger: 'blur' }],
+                six: [{ required: true, message: '请选择性别', trigger: 'blur' }]
             }
         };
     },
+    methods: {
+        handleOnok() {
+            this.$refs['ruleForm'].validate((valid) => {
+                if (valid) {
+                    this.modalVsible = false;
+                } else {
+                    this.$message.error('请正确输入结果');
+                }
+            })
+        },
+        handleOnCanCel() {
+            this.$refs['ruleForm'].validate((valid) => {
+                if (valid) {
+                    this.modalVsible = false;
+                } else {
+                    this.$message.error('请正确输入结果');
+                }
+            })
+        },
+        handleDeal(item) {
+            this.form = item;
+            this.modalVsible = true;
+        }
+    },
     mounted() {
-        axios.get('/user/infoList').then(res => {
+        axios.get('/user/infoList').then((res) => {
             this.tableData = res.data;
-        })
+        });
     }
 };
 </script>
+<style scoped>
+.form-box {
+    width: 455px;
+    margin: 0 auto;
+}
+.dialog-footer {
+    text-align: center;
+}
+</style>
