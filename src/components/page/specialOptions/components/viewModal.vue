f4d85d8cc059a27a4cfb9a4995a7b197863df8f1
ludan <1107743896@qq.com>
2020-09-01 11:24:07 +0800

    v-model写入

---
+++ src/components/page/specialOptions/components/viewModal.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input :value="text1" @input="$emit('change', $event.target.value)" class="input"/>
+    </div>
+</template>
+
+<script>
+export default {
+    model: {
+        prop: 'text1',
+        event: 'change'
+        
+    },
+    props: {
+        text1: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+.input {
+    width: 95%;
+    line-height: 32px;
+    border: 1px solid #ddd;
+    border-radius: 2.5px;
+    padding-left: 12px;
+}
+</style>
--- src/components/page/specialOptions/index.vue
+++ src/components/page/specialOptions/index.vue
@@ -4,21 +4,22 @@
         <div class="container">
             <el-collapse v-model="activeNames" @change="handleChange">
                 <el-collapse-item title="组件中定义v-modal" name="1">
-                   
+                    <span>{{name}}</span>
+                    <viewModal v-model="name"/>
                 </el-collapse-item>
-                <el-collapse-item title="组件中使用$nexttick" name="2">
+                <el-collapse-item title="组件中使用$nexttick" name="2" >
                     
                 </el-collapse-item>
-                <el-collapse-item title="动态组件component/异步组件加载" name="3">
+                <el-collapse-item title="动态组件component/异步组件加载" name="3" >
                    
                 </el-collapse-item>
-                <el-collapse-item title="公用法法mixin用法" name="4">
+                <el-collapse-item title="公用法法mixin用法" name="4" >
                     
                 </el-collapse-item>
                 <el-collapse-item title="slot插槽用法" name="4">
                     
                 </el-collapse-item>
-                <el-collapse-item title="缓存组件keep-alive" name="4">
+                <el-collapse-item title="缓存组件keep-alive" name="4" >
                     
                 </el-collapse-item>
             </el-collapse>
@@ -28,15 +29,18 @@
 
 <script>
 import breadCrumb from '../storeManagement/breadcrumb';
+import viewModal from './components/viewModal.vue';
 export default {
     data() {
         return {
+            name: '这里输入数据',
             activeNames: ['1'],
             title: ['首页', 'vue特殊交易']
         };
     },
     components: {
-        breadCrumb
+        breadCrumb,
+        viewModal
     },
     methods: {
         handleChange(val) {
@@ -45,3 +49,10 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.title-header{
+    font-size: 16px;
+    font-weight: 600;
+}
+</style>>
