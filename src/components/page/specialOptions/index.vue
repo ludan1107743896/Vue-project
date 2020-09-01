ab5438d0ea6091c2e2f938cfcad21712bb3527d5
ludan <1107743896@qq.com>
2020-09-01 10:03:39 +0800

    特殊vue用法使用展示面板展示

--- src/components/page/specialOptions/index.vue
+++ src/components/page/specialOptions/index.vue
@@ -2,8 +2,27 @@
     <div>
         <bread-crumb :titleList="title"></bread-crumb>
         <div class="container">
-            123456
-        </div>     
+            <el-collapse v-model="activeNames" @change="handleChange">
+                <el-collapse-item title="组件中定义v-modal" name="1">
+                   
+                </el-collapse-item>
+                <el-collapse-item title="组件中使用$nexttick" name="2">
+                    
+                </el-collapse-item>
+                <el-collapse-item title="动态组件component/异步组件加载" name="3">
+                   
+                </el-collapse-item>
+                <el-collapse-item title="公用法法mixin用法" name="4">
+                    
+                </el-collapse-item>
+                <el-collapse-item title="slot插槽用法" name="4">
+                    
+                </el-collapse-item>
+                <el-collapse-item title="缓存组件keep-alive" name="4">
+                    
+                </el-collapse-item>
+            </el-collapse>
+        </div>
     </div>
 </template>
 
@@ -12,11 +31,17 @@ import breadCrumb from '../storeManagement/breadcrumb';
 export default {
     data() {
         return {
-            title: ['首页', 'vue特殊交易'],
-        }
+            activeNames: ['1'],
+            title: ['首页', 'vue特殊交易']
+        };
     },
     components: {
-        breadCrumb,
+        breadCrumb
+    },
+    methods: {
+        handleChange(val) {
+            console.log(val);
+        }
     }
-}
+};
 </script>
